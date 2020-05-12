ac849326b5f48544bae96b219a3ab130d76863e8
Ebaa Abdelfadeel <ebaa.abdelfadeel@gmail.com>
2020-05-12 22:11:13 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,5 +1,5 @@
 def reverse_each_word(sentence)
   array = sentence.split(/ /)
-  new_array = array.collect{|word| word.reverse}
-  new_array.join(" ")
+  array.collect{|word| word.reverse}.join(" ")
+  
 end
